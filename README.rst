10236137b2b77d8f258856f0b1865b8b9c524c23
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-02-18 12:15:02 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,25 @@
+====================
+ulearn.js
+====================
+
+uLearn ECMAScript dedicated package
+
+How it works
+============
+
+...
+
+
+Installation
+============
+
+To install `ulearn.js` you simply add ``ulearn.js``
+to the list of eggs in your buildout, run buildout and restart Plone.
+Then, install `ulearn.js` using the Add-ons control panel.
+
+
+Configuration
+=============
+
+...
+
---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+README = open("README.rst").read()
+HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
+
+setup(name='ulearn.js',
+      version=version,
+      description="uLearn ECMAScript dedicated package",
+      long_description=README + "\n" + HISTORY,
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='JS ECMASCript uLearn Genweb',
+      author='Victor Fernandez de Alba',
+      author_email='sneridagh@gmail.com',
+      url='https://github.com/UPCnet/ulearn.js',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ulearn'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require={'test': ['plone.app.testing',
+                               'plone.app.testing[robot]>=4.2.2',
+                               'plone.app.robotframework[debug]',]},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )

eff7f826ad974baa8bab4f722b8aca9b5a79d8ce
Pilar Marinas <pilar.marinas@upcnet.es>
2015-04-27 12:30:02 +0200

    Al clicar sobre cualquier dato usuario rehace searchUser

--- ulearn/js/legacy/ulearn.js
+++ ulearn/js/legacy/ulearn.js
@@ -389,4 +389,36 @@ $(document).ready(function (event) {
 
     });
 
+    //Al clicar sobre cualquier campo de los datos del usuario en searchUsers
+    //(ubicació, location, telefon, email) rehace la busqueda con ese valor
+
+    $("#userlist").on("click", ".ubicacio .fa-user", function (event) {
+        var query = $(this).parent().find('span').html()
+        $.get(window.location.href.match(/(.*\/)/)[0] + '/searchUser', { search: query }, function(data) {
+            $('#userlist').html(data);
+        });
+    });
+
+    $("#userlist").on("click", ".location .fa-building-o", function (event) {
+        var query = $(this).parent().find('span').html()
+        $.get(window.location.href.match(/(.*\/)/)[0] + '/searchUser', { search: query }, function(data) {
+            $('#userlist').html(data);
+        });
+    });
+
+
+    $("#userlist").on("click", ".telefon .fa-mobile", function (event) {
+        var query = $(this).parent().find('span').html()
+        $.get(window.location.href.match(/(.*\/)/)[0] + '/searchUser', { search: query }, function(data) {
+            $('#userlist').html(data);
+        });
+    });
+
+    $("#userlist").on("click", ".email .fa-envelope", function (event) {
+        var query = $(this).parent().find('span').html()
+        $.get(window.location.href.match(/(.*\/)/)[0] + '/searchUser', { search: query }, function(data) {
+            $('#userlist').html(data);
+        });
+    });
+
 });

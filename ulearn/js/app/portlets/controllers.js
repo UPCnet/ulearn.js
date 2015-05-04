83cd21f72f8008c464363cdbed8095bfb7b54ad3
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-05-04 12:06:52 +0200

    Complete the change community view, add translations

--- ulearn/js/app/portlets/controllers.js
+++ ulearn/js/app/portlets/controllers.js
@@ -9,34 +9,34 @@
  */
 
 
-GenwebApp.controller('profilePortlet', ['ngDialog', function (ngDialog) {
+GenwebApp.controller('profilePortlet', [function () {
   var self = this;
 
-  self.openChangeTypeModal = function (community_type, community_hash) {
-        ngDialog.open({
-            template: '/++ulearn++app/portlets/change_community_type.html',
-            className: 'ngdialog-theme-default',
-            controller: ['$scope', 'plonePortalURL', 'MAXInfo', function ($scope, plonePortalURL, MAXInfo) {
-                var self = this;
-                $scope.selected = $scope.ngDialogData.community_type;
-                $scope.changeCommunityType = function (selected) {
-                    var data = {community_type: selected};
-                    debugger
-                    $http.put(
-                        plonePortalURL + 'api/communities/' + $scope.ngDialogData.community_hash,
-                        data,
-                        {headers:MAXInfo.headers}
-                    .then()
-                    );
-                };
-            }],
-            data: {community_type: community_type, community_hash: community_hash},
-            cache: false
-        });
-      };
+  // self.openChangeTypeModal = function (community_type, community_hash) {
+  //       ngDialog.open({
+  //           template: '/++ulearn++app/portlets/change_community_type.html',
+  //           className: 'ngdialog-theme-default',
+  //           controller: ['$scope', 'plonePortalURL', 'MAXInfo', function ($scope, plonePortalURL, MAXInfo) {
+  //               var self = this;
+  //               $scope.selected = $scope.ngDialogData.community_type;
+  //               $scope.changeCommunityType = function (selected) {
+  //                   var data = {community_type: selected};
+  //                   debugger
+  //                   $http.put(
+  //                       plonePortalURL + 'api/communities/' + $scope.ngDialogData.community_hash,
+  //                       data,
+  //                       {headers:MAXInfo.headers}
+  //                   .then()
+  //                   );
+  //               };
+  //           }],
+  //           data: {community_type: community_type, community_hash: community_hash},
+  //           cache: false
+  //       });
+  //     };
 }]);
 
-GenwebApp.controller('profilePortletModal', ['$scope', '$http', '$timeout', '$window', 'plonePortalURL', 'MAXInfo', 'SweetAlert', function ($scope, $http, $timeout, $window, plonePortalURL, MAXInfo, SweetAlert) {
+GenwebApp.controller('profilePortletModal', ['$scope', '$http', '$timeout', '$window', 'plonePortalURL', 'MAXInfo', 'SweetAlert', '$translate', function ($scope, $http, $timeout, $window, plonePortalURL, MAXInfo, SweetAlert, $translate) {
   $scope.selected = $scope.ngDialogData.community_type;
   $scope.changeCommunityType = function (selected) {
     var data = {community_type: selected};
@@ -45,11 +45,19 @@ GenwebApp.controller('profilePortletModal', ['$scope', '$http', '$timeout', '$wi
       data,
       {headers:MAXInfo.headers})
     .success(function() {
-      SweetAlert.swal({title:'Good job!', description:'You clicked the button!', type:'success', timer: 2000});
       $scope.closeThisDialog();
       $timeout(function () { $window.location.reload(); }, 700);
     })
-    .error(function () {SweetAlert.swal({title:'Error', description:'Something wrong happened with the backend, please try again', type:'error', timer: 2000});});
+    .error(function () {
+      $translate(['CHANGECOMMUNITYTYPE_VIEW.ERROR'])
+        .then(function (translations) {
+          SweetAlert.swal({
+            title:'Error',
+            description: translations['CHANGECOMMUNITYTYPE_VIEW.ERROR'],
+            type:'error',
+            timer: 2000});
+        });
+    });
   };
 
 }]);

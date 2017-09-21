2542ec9f65b2e6bc7804e812714355d17d51a144
Roberto Diaz <roberto.diaz@upcnet.es>
2017-09-21 17:34:48 +0200

    Add NewsInApp Toggle and removed commented code

--- ulearn/js/legacy/ulearn.js
+++ ulearn/js/legacy/ulearn.js
@@ -3,22 +3,6 @@ $(document).ready(function (event) {
     jarn.i18n.loadCatalog('ulearn');
     _ulearn_i18n = jarn.i18n.MessageFactory('ulearn');
 
-    // customizedInput = false;
-
-    // $('#addModal').on('shown', function (e) {
-    //     if (!customizedInput) {
-    //         customizedInput = true;
-    //         $('[type=file]').customFileInput();
-    //         }
-    // });
-
-    // $('#editModal').on('shown', function (e) {
-    //     if (!customizedInput) {
-    //         customizedInput = true;
-    //         $('[type=file]').customFileInput();
-    //         }
-    // });
-
     // Favorites on dexterity objects
     $('#object-favorite').on('click', '.favorite', function(event) {
       event.preventDefault();
@@ -31,103 +15,10 @@ $(document).ready(function (event) {
       }
     });
 
-    // Favorites on community list
-    // $('#communities-view').on('click', '.favorite', function(event) {
-    //   event.preventDefault();
-    //   var community_url = $(this).data()['community'];
-    //   $.get(community_url + '/toggle-favorite');
-    //   if ($('i', this).hasClass('fa-star')) {
-    //     $('i', this).addClass('fa-star-o').removeClass('fa-star');
-    //   } else {
-    //     $('i', this).addClass('fa-star').removeClass('fa-star-o');
-    //   }
-    // });
-
-    // Delete community on community list
-    // $('#communities-view').on('click', '.delete', function(event) {
-    //     event.preventDefault();
-    //     var $this = $(this);
-    //     alertify.confirm(_ulearn_i18n("Si cliqueu aquí, esborrareu la comunitat ") + $this.data()['name'] + '"?', function (e) {
-    //         if (e) {
-    //             // user clicked "ok"
-    //             url = $this.attr('href');
-    //             $.ajax({
-    //                 type: "POST",
-    //                 url: url,
-    //                 data: {
-    //                 'form.submitted': '1',
-    //                 '_authenticator': $this.data()['authenticator']
-    //                 },
-    //                 error: function() {
-    //                     alertify.error(_ulearn_i18n("Error when removing community"));
-    //                 },
-    //                 success: function() {
-    //                     $this.parent().parent().parent().remove();
-    //                     alertify.success(_ulearn_i18n("Successfully removed"));
-    //                 }
-    //             });
-    //         } else {
-    //             // user clicked "cancel"
-    //         }
-    //     });
-    // });
-
-    // Subscribe to community on community list
-    // $('#communities-view').on('click', '.subscribe', function(event) {
-    //     event.preventDefault();
-    //     var $this = $(this);
-    //     var msgalert = '';
-
-    //     if ($('i', $this).hasClass('fa-check-square-o')) {
-    //         msgalert = "Voleu desubscrivir-vos de la comunitat ";
-    //     } else {
-    //         msgalert = "Voleu subscrivir-vos a la comunitat ";
-    //     }
-
-    //     alertify.confirm(_ulearn_i18n(msgalert) + '"' + $this.data()['name'] + '"?', function (e) {
-    //         if (e) {
-    //             // user clicked "ok"
-    //             community_url = $this.data()['community'];
-    //             $.ajax({
-    //                 type: "GET",
-    //                 url: community_url + "/toggle-subscribe",
-    //                 error: function() {
-    //                     alertify.error(-_ulearn_i18n("Error when (un)subscribing to the community"));
-    //                 },
-    //                 success: function() {
-    //                     if ($('i', $this).hasClass('fa-check-square-o')) {
-    //                         $('i', $this).addClass('fa-square-o').removeClass('fa-check-square-o');
-    //                         $('.favorite').addClass('favoritedisabled').removeClass('favorite');
-    //                         alertify.success(_ulearn_i18n("Successfully unsubscribed"));
-    //                     } else {
-    //                         $('i', $this).addClass('fa-check-square-o').removeClass('fa-square-o');
-    //                         $('.favoritedisabled').addClass('favorite').removeClass('favoritedisabled');
-    //                         alertify.success(_ulearn_i18n("Successfully subscribed"));
-    //                     }
-    //                 }
-    //             });
-    //         } else {
-    //             // user clicked "cancel"
-    //         }
-    //     });
-    // });
-
-    // $('.portaltype-plone-site a[data-toggle="tab"]').on('show', function (e) {
-    //     targetid = $(this).data('target');
-    //     remote = $(targetid).data('remote');
-    //     if (remote) {
-    //         $(targetid).load(portal_url + "/" + remote, function (event) {
-    //             $('.sortablelist').mixitup({layoutMode: 'list'});
-    //         });
-    //     }
-    //     $('#menusup .active').removeClass('active');
-    //     $(e.target.parentElement.parentElement).addClass('active');
-    // });
 
     $(function(){
 
         var $username = $("#box_perfil #user h2");
-
         var $letters = $username.text().length;
 
         if (($letters >= 1) && ($letters < 10)) {
@@ -148,14 +39,6 @@ $(document).ready(function (event) {
 
     });
 
-    // Community search
-    // $('#searchinputcommunities .searchInput').on('keyup', function(event) {
-    //     var query = $(this).val();
-    //     $('.listingBar').hide();
-    //     $.get(portal_url + '/search-communities-ajax', { q: query }, function(data) {
-    //         $('#communitylist').html(data);
-    //     });
-    // });
 
     var delay = (function(){
       var timer = 0;
@@ -165,19 +48,6 @@ $(document).ready(function (event) {
       };
     })();
 
-   /* // User search
-    $('#searchinputusers .searchInput').on('keyup', function(event) {
-        var query = $(this).val();
-        if (query.length > 2 || query.length === 0) {
-            delay(function(){
-                $('.listingBar').hide();
-                $.get(window.location.href.match(/(.*\/)/)[0] + '/searchUser', { search: query }, function(data) {
-                    $('#userlist').html(data);
-                });
-            }, 1000 );
-        }
-    });
-*/
     // # of thinnkinns updater
     $(window).on('maxui-posted-activity', function(event) {
         int_activities = parseInt($('.currentactivity').text(), 10);
@@ -221,24 +91,6 @@ $(document).ready(function (event) {
         });
     };
 
-    // $("#subscribealert").on("click", "a", function (event) {
-    //     var options = {wsURL: "/toggle-subscribe"};
-    //     subscribe_to_community(event, options);
-    // });
-
-    // $("#subscribeupgradealert").on("click", "a", function (event) {
-    //     var options = {wsURL: "/upgrade-subscribe"};
-    //     subscribe_to_community(event, options);
-    // });
-
-    // Prevent click on calendar events to allow popover
-  /*  $('.cal_has_events').click(function (e) {
-        e.preventDefault();
-        $('.popover-content').off('click').on('click', 'a' , function() {
-            window.location=this.href
-        });
-
-    });*/
 
     $(".magrada a").on("click", function (event) {
         event.preventDefault();
@@ -251,7 +103,6 @@ $(document).ready(function (event) {
                 alertify.error(_ulearn_i18n("Error when (un)like the proposal"));
             },
             success: function() {
-                // debugger;
                 if ($('i', $anchor).hasClass('fa-heart-o')) {
                     $('i', $anchor).addClass('fa-heart').removeClass('fa-heart-o');
                     alertify.success(_ulearn_i18n("Gràcies per donar suport a aquesta idea."));
@@ -263,7 +114,6 @@ $(document).ready(function (event) {
                 }
             }
         });
-
     });
 
     $(".mhiapunto a").on("click", function (event) {
@@ -277,7 +127,6 @@ $(document).ready(function (event) {
                 alertify.error(_ulearn_i18n("Error when (un)join the proposal"));
             },
             success: function() {
-                // debugger;
                 if ($('i', $anchor).hasClass('fa-sign-out')) {
                     $('i', $anchor).addClass('fa-sign-in').removeClass('fa-sign-out');
                     alertify.success(_ulearn_i18n("Heu cancel·lat la implicació a aquesta proposta."));
@@ -289,7 +138,6 @@ $(document).ready(function (event) {
                 }
             }
         });
-
     });
 
     // NX24 like popover specific
@@ -378,7 +226,6 @@ $(document).ready(function (event) {
           if (!_.findWhere(window._gw_typeahead_last_result, {'title': text})) {
               window.location.href = $typeahead_dom.attr('data-search-url') + '?SearchableText=' + text + '&path=' + cf;
           }
-
       }
     })
     .on("typeahead:selected", function(event, suggestion, dataset) {
@@ -386,25 +233,31 @@ $(document).ready(function (event) {
         event.stopPropagation();
         event.stopImmediatePropagation();
         window.location.href = suggestion.itemUrl;
-
     });
 
-$('#toggle-flash').on('click', '.sliderbutton', function(event) {
-  var dexterity_url = $(this).data()['dextobject'];
-  $.post(dexterity_url + '/toggle_flash');
-});
+    // Toggle Flash in Dexterity Objects
+    $('#toggle-flash').on('click', '.sliderbutton', function(event) {
+      var dexterity_url = $(this).data()['dextobject'];
+      $.post(dexterity_url + '/toggle_flash');
+    });
 
-$('#toggle-important').on('click', '.sliderbutton', function(event) {
-  var dexterity_url = $(this).data()['dextobject'];
-  $.post(dexterity_url + '/toggle_important');
-});
+    // Toggle Important in Dexterity Objects
+    $('#toggle-important').on('click', '.sliderbutton', function(event) {
+      var dexterity_url = $(this).data()['dextobject'];
+      $.post(dexterity_url + '/toggle_important');
+    });
 
-$('#toggle-outoflist').on('click', '.sliderbutton', function(event) {
-  var dexterity_url = $(this).data()['dextobject'];
-  $.post(dexterity_url + '/toggle_outoflist');
-});
+    // Toggle List in Dexterity Objects
+    $('#toggle-outoflist').on('click', '.sliderbutton', function(event) {
+      var dexterity_url = $(this).data()['dextobject'];
+      $.post(dexterity_url + '/toggle_outoflist');
+    });
 
-
+    // Toggle News in App in Dexterity News Items
+    $('#toggle-newsinapp').on('click', '.sliderbutton', function(event) {
+      var dexterity_url = $(this).data()['dextobject'];
+      $.post(dexterity_url + '/toggle_news_in_app');
+    });
 
 
 });
